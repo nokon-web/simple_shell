dd446a0ec77baeaf1195d3aaffcc52398e78cabc
nokon <nokon@gmail.com>
2023-04-28 14:09:30 -0700

    errors file

--- errors.c
+++ errors.c
@@ -1,85 +1,85 @@
 #include "shell.h"
 
 /**
- *_eputs - prints an input string
- * @str: the string to be printed
+ *puts_e - prints an input
+ *@word: the string to be printed
  *
  * Return: Nothing
  */
-void _eputs(char *str)
+void puts_e(char *word)
 {
 	int i = 0;
 
-	if (!str)
+	if (!word)
 		return;
-	while (str[i] != '\0')
+	while (word[i] != '\0')
 	{
-		_eputchar(str[i]);
+		putchar_e(word[i]);
 		i++;
 	}
 }
 
 /**
- * _eputchar - writes the character c to stderr
- * @c: The character to print
+ * putchar_e - writes the character c to stderr
+ * @letter: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On sucess 1.
+ * On error, -1 is returned, and errno is set appropraitely.
  */
-int _eputchar(char c)
+int putchar_e(char letter)
 {
-	static int i;
+	static int idx;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (letter == BUF_FLUSH || idx >= WRITE_BUF_SIZE)
 	{
-		write(2, buf, i);
-		i = 0;
+		write(2, buf, idx);
+		idx = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	if (letter != BUF_FLUSH)
+		buf[idx++] = letter;
 	return (1);
 }
 
 /**
- * _putfd - writes the character c to given fd
+ * put_fd - writes the character c to given fd
  * @c: The character to print
  * @fd: The filedescriptor to write to
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropraitely.
  */
-int _putfd(char c, int fd)
+int put_fd(char c, int fd)
 {
-	static int i;
+	static int idx;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || idx >= WRITE_BUF_SIZE)
 	{
-		write(fd, buf, i);
-		i = 0;
+		write(fd, buf, idx);
+		idx = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buf[idx++] = c;
 	return (1);
 }
-
 /**
- *_putsfd - prints an input string
+ * puts_fd - prints an input string
  * @str: the string to be printed
  * @fd: the filedescriptor to write to
  *
- * Return: the number of chars put
+ * Return: the number of chars to put
  */
-int _putsfd(char *str, int fd)
+int puts_fd(char *str, int fd)
 {
-	int i = 0;
+	int idx = 0;
 
 	if (!str)
 		return (0);
 	while (*str)
 	{
-		i += _putfd(*str++, fd);
+		idx += put_fd(*str++, fd);
 	}
-	return (i);
+	return (idx);
 }
+
