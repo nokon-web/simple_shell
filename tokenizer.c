afd7a3cd5d4c9955d4fab12ad596b99bea0a4e9a
nokon <nokon@gmail.com>
2023-04-28 14:16:33 -0700

    tokenizer file

--- tokenizer.c
+++ tokenizer.c
@@ -4,7 +4,6 @@
  * **strtow - splits a string into words. Repeat delimiters are ignored
  * @str: the input string
  * @d: the delimeter string
- * Return: a pointer to an array of strings, or NULL on failure
  */
 
 char **strtow(char *str, char *d)
