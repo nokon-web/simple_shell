0c5bffbf64f722a8bad6d777b2cb887f67cf22d5
nokon <nokon@gmail.com>
2023-04-28 13:52:36 -0700

    lists file

--- lists.c
+++ lists.c
@@ -2,8 +2,8 @@
 
 /**
  * add_node - adds a node to the start of the list
- * @head: address of pointer to head node
- * @str: str field of node
+ * @ head: address of pointer to head node
+ * @ str: str field of node
  * @num: node index used by history
  *
  * Return: size of list
